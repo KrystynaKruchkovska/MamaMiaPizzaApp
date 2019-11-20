240aa83b47b5cfaa629597b314ef682821a8f361
Krystyna Kruchkovska <kruchok883@gmail.com>
2019-11-20 14:26:04 +0100

    Basic implementation

--- ContentView.swift
+++ ContentView.swift
@@ -10,12 +10,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            ZStack {
+                Image("pizza_logo")
+                .resizable()
+                .scaledToFit()
+                
+                VStack {
+                    HStack{
+                        Spacer()
+                        Text("Here you are")
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                }
+               
+            } .background(Color.red)
+            VStack {
+
+                
+                Text("Order Pizza!")
+                    .font(.largeTitle)
+                Spacer()
+                HStack(alignment: .top, spacing: 15) {
+                    Image("pizza")
+                        .resizable()
+                        .scaledToFit()
+                    Text("Chicken pizza")
+                    Spacer()
+                }
+                Text("Your order")
+                HStack(alignment: .firstTextBaseline){
+                    Text("Your ordered item")
+                    Spacer()
+                    Text("$ 0.00")
+                    
+                }
+            }
+            .padding()
+            
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+                .colorScheme(.dark)
+                .background(Color.black )
+                .previewDevice("iPad Pro (9.7-inch)")
+        }
     }
 }
+
