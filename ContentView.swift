f9850344091573525ece3421d8d8553fd6bded1f
Krystyna Kruchkovska <kruchok883@gmail.com>
2019-11-21 10:58:44 +0100

    added collections views

--- ContentView.swift
+++ ContentView.swift
@@ -11,46 +11,49 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            ZStack {
-                Image("pizza_logo")
+            Image("pizza_logo")
                 .resizable()
                 .scaledToFit()
-                
-                VStack {
-                    HStack{
-                        Spacer()
-                        Text("Here you are")
-                            .font(.largeTitle)
-                    }
-                    Spacer()
-                }
-               
-            } .background(Color.red)
-            VStack {
-
-                
-                Text("Order Pizza!")
-                    .font(.largeTitle)
-                Spacer()
+                .frame(width: 300, height: 100, alignment: .center)
+            
+            Text("Order Pizza!")
+                .font(.largeTitle)
+            
+            Spacer()
+            Text("Menu")
+            
+            List(0..<10) { item in
                 HStack(alignment: .top, spacing: 15) {
                     Image("pizza")
                         .resizable()
                         .scaledToFit()
-                    Text("Chicken pizza")
-                    Spacer()
+                        .frame(width: 150, height: 100, alignment: .center)
+                    VStack {
+                        Text("Chicken pizza")
+                        
+                        HStack {
+                            ForEach(0..<4) { item in
+                                Image("pizza_slice")
+                                .resizable()
+                                .scaledToFit()
+                            }
+                        }
+                    }
                 }
-                Text("Your order")
-                HStack(alignment: .firstTextBaseline){
+            }
+            Text("Your order")
+            List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/){ item in
+                HStack(alignment: .top, spacing: 15){
                     Text("Your ordered item")
                     Spacer()
                     Text("$ 0.00")
-                    
                 }
+                
             }
-            .padding()
-            
-        }
+            Spacer()
+        }.padding()
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
